d6d84101c8aff75b6613cd9b3149b94dd6efb1f4
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-03-04 01:08:44 -0600

    updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,24 @@
-console.log("hello");
+//setting up svg wrapper
+let svgwidth = 960;
+let svgheight = 500;
+
+let margin = {
+    top: 20,
+    right: 40,
+    bottom: 80,
+    left: 100
+};
+
+let width = svgwidth - margin.left - margin.right;
+let height = svgheight - margin.top - margin.bottom;
+
+let svg = d3
+    .select("#scatter")
+    .append("svg")
+    .attr("width", svgwidth)
+    .attr("height", svgheight);
+
+
 
 // loading csv data
 d3.csv("assets/data/data.csv").then(function(data) {
