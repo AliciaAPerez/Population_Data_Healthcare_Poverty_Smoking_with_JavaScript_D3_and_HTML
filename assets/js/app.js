faf674ab22ae16e405fbf95c4649892e05868cb5
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-03-04 02:53:30 -0600

    updating

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 //setting up svg wrapper
 let svgwidth = 960;
 let svgheight = 500;
@@ -17,6 +18,41 @@ let svg = d3
     .append("svg")
     .attr("width", svgwidth)
     .attr("height", svgheight);
+
+let chartgroup = svg.append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+let chosenXaxis = "proverty";
+    
+function xScale(data, chosenXaxis) {
+    let xLinearScale = d3.scaleLinear()
+    .domain([d3.min(data, d => d[chosenXaxis]),
+    d3.max(data, d => d[chosenXaxis])
+    ])
+    .range([0, width]);
+
+    return xLinearScale;
+
+}
+
+function renderAxes(newXScale, xAxis) {
+    let bottomaxis = d3.axisBottom(newXScale);
+
+    xAxis.transition()
+        .duration(1000)
+        .call(bottomaxis);
+    
+    return xAxis;
+}
+
+function renderCircles(circlesGroup, newXScale, chosenXaxis) {
+    circlesGroup.transition()
+        .duration(1000)
+        .attr("cx", d => newXScale(d[chosenXaxis]));
+    
+    return circlesGroup;
+}
+
 
 
 
